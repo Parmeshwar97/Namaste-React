58e169a922e3a2ee0710c643dd5d9469822f604e
Parmeshwar <parmeshwarpatil9763@gmail.com>
2024-11-26 05:56:49 +0530

    episode-8

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,15 @@
-import Header from "./Header";
-
+import Counter from "../../class-componets/Counter";
 const About = () => {
   return (
-    <div>
-      <h1>This is About Page</h1>
-      <h3>
-        Consistency is key to growth the difference is nothing in consistency!
-      </h3>
-    </div>
+    <>
+      <div>
+        <h1>This is About Page</h1>
+        <h3>
+          Consistency is key to growth the difference is nothing in consistency!
+        </h3>
+      </div>
+      <Counter name='pamu'/>
+    </>
   );
 };
 export default About;
